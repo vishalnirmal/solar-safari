f8e3d4c8d274b5729432f02a2c184154ba69c24b
Vishal Nirmal <vishalnirmal53@gmail.com>
2023-08-06 15:53:50 +0530

    feat: style configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,49 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "space-black": "var(--space-black)",
+        "space-blue": "var(--space-blue)",
+        "space-white": "var(--space-white)",
+      },
+      fontFamily: {
+        bellefair: ["Bellefair", "serif"],
+        barlow: ["Barlow Condensed", "sans-serif"],
+      },
+      fontSize: {
+        "heading-1": ["150px", "1em"],
+        "heading-2": ["100px", "1em"],
+        "heading-3": ["56px", "1em"],
+        "heading-4": ["32px", "1em"],
+        "heading-5": [
+          "28px",
+          {
+            letterSpacing: "4.75px",
+            lineHeight: "1em",
+            fontWeight: "400",
+          },
+        ],
+        "sub-heading-1": ["28px", "1em"],
+        "sub-heading-2": [
+          "14px",
+          {
+            letterSpacing: "2.35px",
+            lineHeight: "1em",
+            fontWeight: "400",
+          },
+        ],
+        nav: [
+          "16px",
+          {
+            letterSpacing: "2.7px",
+            lineHeight: "1em",
+            fontWeight: "400",
+          },
+        ],
+      },
+    },
+  },
   plugins: [],
 };
